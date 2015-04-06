535eb4394808bad44ba0b8620289dcce3af5b5ce
aurielfournier <aurielfournier@gmail.com>
2015-04-06 11:16:57 -0500

    added graphs, converted to UTM

--- quan_tech_final_project.R
+++ quan_tech_final_project.R
@@ -1,9 +1,16 @@
+library(ggplot2)
+library(rgdal)
+library(gridExtra)
 
 dat <- read.csv("all_birds.csv")
+dat <- na.omit(dat)
 
+utm <- as.data.frame(project(cbind(dat$long, dat$lat), "+proj=utm +zone=15 ellps=WGS84"))
+colnames(utm) <- c("utm_w","utm_n")
+
+dat <- cbind(dat, utm)
 dat4 <- dat[dat$year==2014,]
 
-dat4 <- na.omit(dat4)
 
 jdate4 <- unique(dat4$jdate)
 
@@ -32,9 +39,9 @@ for (i in c(1:14,16:21,24:40)) {
   a <- df[df$obs=="N",]
   b <- df[df$obs=="A",]
             for(j in 1:nrow(a)){
-            c <- b[which.min(sqrt(abs(a[j,"lat"]-b[,"lat"])^2 + abs(a[j,"long"]-b[,"long"])^2)),]
+            c <- b[which.min(sqrt(abs(a[j,"utm_n"]-b[,"utm_n"])^2 + abs(a[j,"utm_w"]-b[,"utm_w"])^2)),]
             d <- a[j,]
-            c$dist <- min(sqrt(abs(a[j,"lat"]-b[,"lat"])^2 + abs(a[j,"long"]-b[,"long"])^2))
+            c$dist <- min(sqrt(abs(a[j,"utm_n"]-b[,"utm_n"])^2 + abs(a[j,"utm_w"]-b[,"utm_w"])^2))
             dist4[[j]] <- cbind(c,d)
       }
   newdf[[i]] <- do.call(rbind, dist4)
@@ -42,3 +49,40 @@ for (i in c(1:14,16:21,24:40)) {
 
 
 dist <- do.call(rbind, newdf)
+
+
+nv <- ggplot(data=dist[dist$canwr=="nvca",])+
+  geom_point(aes(x=utm_w, y=utm_n, colour=dist))+
+  ggtitle("nv")
+sc <- ggplot(data=dist[dist$canwr=="scnwr",])+
+  geom_point(aes(x=utm_w, y=utm_n, colour=dist))+
+  ggtitle("sc")
+fg <- ggplot(data=dist[dist$canwr=="fgca",])+
+  geom_point(aes(x=utm_w, y=utm_n, colour=dist))+
+  ggtitle("fg")
+sl <- ggplot(data=dist[dist$canwr=="slnwr",])+
+  geom_point(aes(x=utm_w, y=utm_n, colour=dist))+
+  ggtitle("sl")
+ts <- ggplot(data=dist[dist$canwr=="tsca",])+
+  geom_point(aes(x=utm_w, y=utm_n, colour=dist))+
+  ggtitle("ts")
+bk <- ggplot(data=dist[dist$canwr=="bkca",])+
+  geom_point(aes(x=utm_w, y=utm_n, colour=dist))+
+  ggtitle("bk")
+cc <- ggplot(data=dist[dist$canwr=="ccnwr",])+
+  geom_point(aes(x=utm_w, y=utm_n, colour=dist))+
+  ggtitle("cc")
+dc <- ggplot(data=dist[dist$canwr=="dcca",])+
+  geom_point(aes(x=utm_w, y=utm_n, colour=dist))+
+  ggtitle("dc")
+os <- ggplot(data=dist[dist$canwr=="osca",])+
+  geom_point(aes(x=utm_w, y=utm_n, colour=dist))+
+  ggtitle("os")
+tm <- ggplot(data=dist[dist$canwr=="tmpca",])+
+  geom_point(aes(x=utm_w, y=utm_n, colour=dist))+
+  ggtitle("tm")
+
+grid.arrange(nv, sc, fg, sl, ts, bk, cc, dc, os, tm, ncol=2)
+
+
+hist(dist[dist$dist<=1500,]$dist, breaks=100)
